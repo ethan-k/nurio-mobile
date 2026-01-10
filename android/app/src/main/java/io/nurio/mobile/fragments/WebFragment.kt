f45cdaeba97a8d582346b7acfb78cf69e5404fc7
Eunseok Kang <6640728+ethan-k@users.noreply.github.com>
2026-01-10 13:56:02 +0900

    revert: show native toolbar for proper navigation
    Follow Hotwire best practice - hide web header in native app
    instead of hiding native toolbar.

--- android/app/src/main/java/io/nurio/mobile/fragments/WebFragment.kt
+++ android/app/src/main/java/io/nurio/mobile/fragments/WebFragment.kt
@@ -1,14 +1,7 @@
 package io.nurio.mobile.fragments
 
-import android.os.Bundle
-import android.view.View
 import dev.hotwire.navigation.destinations.HotwireDestinationDeepLink
 import dev.hotwire.navigation.fragments.HotwireWebFragment
 
 @HotwireDestinationDeepLink(uri = "hotwire://fragment/web")
-class WebFragment : HotwireWebFragment() {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        toolbarForNavigation()?.visibility = View.GONE
-    }
-}
+class WebFragment : HotwireWebFragment()
