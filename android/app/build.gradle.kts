fb31fda8091aed015626ae1dcc11ae99525e8ac3
Eunseok Kang <6640728+ethan-k@users.noreply.github.com>
2026-08-30 08:44:51 +0900

    chore(release): bump Nurio Android to 1.0.12 (17)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.nurio.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 16
+        versionCode = 17
         versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
