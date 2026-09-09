cfadfe91ec64fd0f03d6e8ee9f2f812d1c06e6ea
Eunseok Kang <6640728+ethan-k@users.noreply.github.com>
2026-09-09 11:11:25 +0900

    chore(release): bump Nurio Android to 1.0.14 (19)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.nurio.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.0.13"
+        versionCode = 19
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["crashlyticsCollectionEnabled"] = true
